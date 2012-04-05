8ce17f25e6650c281561d004125c2c38f38c38ec
Kayvan Boudai <Kayvan.Boudai@gmail.com>
2012-04-04 17:33:00 -0700

    adding Score and World

---
+++ BlockBlock/src/mygame/World.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import java.util.Queue;
+
+/**
+ *
+ * @author kayvanboudai
+ */
+public class World {
+    private Queue blockQueue;
+    private Score score;
+    private TheGrid theGrid;
+    private Block currentBlock;
+    
+    public World() {}
+    public void update() {}
+    public void draw() {}
+    public void Input(InputState inputState) {} //TODO figure out input
+    
+    
+}
