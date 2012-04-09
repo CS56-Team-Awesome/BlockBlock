23f3be4d420cef9555e3868a2338e0e867e0ec40
Hasen Ahmad <kaizokuace@gmail.com>
2012-04-08 18:13:33 -0700

    started adding world stuff

--- BlockBlock/src/mygame/World.java
+++ BlockBlock/src/mygame/World.java
@@ -4,22 +4,64 @@
  */
 package mygame;
 
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.input.InputManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
 import java.util.Queue;
 
 /**
  *
  * @author kayvanboudai
  */
-public class World {
+public class World extends AbstractAppState{
     private Queue blockQueue;
     private Score score;
     private TheGrid theGrid;
     private Block currentBlock;
     
+    private SimpleApplication app;
+    private Node              rootNode;
+    private AssetManager      assetManager;
+    private AppStateManager   stateManager;
+    private InputManager      inputManager;
+    private ViewPort          viewPort;
+    private BulletAppState    bullet;
+    
     public World() {}
-    public void update() {}
-    public void draw() {}
    //TODO: figure out input and make input state - public void Input(InputState inputState) {} 
+
+    @Override
+    public void cleanup() {
+        super.cleanup();
+    }
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app          = (SimpleApplication) app; // can cast Application to something more specific
+        this.rootNode     = this.app.getRootNode();
+        this.assetManager = this.app.getAssetManager();
+        this.stateManager = this.app.getStateManager();
+        this.inputManager = this.app.getInputManager();
+        this.viewPort     = this.app.getViewPort();
+        this.bullet       = this.stateManager.getState(BulletAppState.class);
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+    }
+
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+    }
     
     
 }
