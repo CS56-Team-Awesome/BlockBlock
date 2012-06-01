3918fd71aefbc61de537b546bd8cb9ea149cefa7
Hasen Ahmad <kaizokuace@gmail.com>
2012-06-01 00:59:30 -0700

    changed block model and texture

--- BlockBlock/src/teamawesome/blockblock/BlockFactory.java
+++ BlockBlock/src/teamawesome/blockblock/BlockFactory.java
@@ -30,7 +30,7 @@ public class BlockFactory {
         this.assetManager = assetManager;
         
         /*--------------------generate block----------------------------------*/ 
-        block = assetManager.loadModel("Models/Block.j3o");
+        block = assetManager.loadModel("Models/Block2.j3o");
         block.setName("Block" + (blockCount++) + "_" + color);
         //Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -40,49 +40,49 @@ public class BlockFactory {
         case Red: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_red.png"));
             mat.setColor("Color", ColorRGBA.Red);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new RedBlockControl());
             break;
         case Blue: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_blue.png"));
             mat.setColor("Color", ColorRGBA.Blue);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new BlueBlockControl());
             break;
         case Black: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_black.png"));
             mat.setColor("Color", ColorRGBA.White);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new BlackBlockControl());
             break;
         case Yellow: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_yellow.png"));
             mat.setColor("Color", ColorRGBA.Yellow);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new YellowBlockControl());
             break;
         case Green: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_green.png"));
             mat.setColor("Color", ColorRGBA.Green);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new GreenBlockControl());
             break;
         case Grey: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_grey.png")); 
             mat.setColor("Color", ColorRGBA.Gray);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new GreyBlockControl());
             break;
         case Orange: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_orange.png"));
             mat.setColor("Color", ColorRGBA.Orange);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new OrangeBlockControl());
             break;
         case Rainbow: 
             //mat.setTexture("DiffuseMap", assetManager.loadTexture("Textures/block_orange.png"));
             mat.setColor("Color", ColorRGBA.Orange);
-            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_blue.png"));
+            mat.setTexture("ColorMap", assetManager.loadTexture("Textures/block_texture2.png"));
             block.addControl(new RainbowBlockControl());
             break;   
         }
